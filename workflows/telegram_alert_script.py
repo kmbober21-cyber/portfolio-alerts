969f6a7da455e28082bd2dc3877c52301db20b9f
kmbober21-cyber <km.bober21@gmail.com>
2026-08-01 21:02:30 -0400

    Update telegram_alert_script.py

--- workflows/telegram_alert_script.py
+++ workflows/telegram_alert_script.py
@@ -1,1 +1,9 @@
+import os, requests
 
+TOKEN=os.environ['TELEGRAM_BOT_TOKEN']
+CHAT_ID=os.environ['TELEGRAM_CHAT_ID']
+text=os.environ.get('ALERT_TEXT','Test alert')
+url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+r=requests.post(url, data={'chat_id':CHAT_ID, 'text':text})
+r.raise_for_status()
+print(r.text)
